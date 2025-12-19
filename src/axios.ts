b16fca36b34af92c13285ffcb537975ea8865bf8
Milan Bairagi <nmilan626@gmail.com>
2025-12-19 18:16:54 +0545

    refactor: change the axios instance to be created as a function.
    This allows the instance to be created with the current access token, refresh token, vault unlock token and setAuthTokens function.
    The refresh token logic has been updated to use the new instance.

--- src/axios.ts
+++ src/axios.ts
@@ -1,58 +1,66 @@
 import axios from "axios";
-import { useAuthCredential } from "./hooks/useAuthCredential";
+import type { AxiosInstance } from "axios";
+
+import type { AuthTokens } from "./contexts/useAuthCredential";
 
 
-const { accessToken, refreshToken, vaultUnlockToken, setAuthTokens } = useAuthCredential();
-const url = import.meta.env.VITE_API_URL;
+type NullableString = string | null;
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 10000,
-});
+const api = (
+  accessToken: NullableString,
+  refreshToken: NullableString,
+  vaultUnlockToken: NullableString,
+  setAuthTokens: (tokens: AuthTokens) => Promise<void>
+): AxiosInstance => {
 
-api.interceptors.request.use(
-  (config) => {
-    if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
+  const instance = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    timeout: 10000,
+  });
+
+  instance.interceptors.request.use(
+    (config) => {
+      (accessToken) && (config.headers.Authorization = `Bearer ${accessToken}`);
+      (vaultUnlockToken) && (config.headers["X-Vault-Unlock-Token"] = vaultUnlockToken);
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
     }
-    if (vaultUnlockToken) {
-      config.headers["X-Vault-Unlock-Token"] = vaultUnlockToken;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+  );
 
-api.interceptors.response.use(
-  (res) => res,
-  async (error) => {
-    const config = error.config;
+  instance.interceptors.response.use(
+    (res) => res,
+    async (error) => {
+      const config = error.config;
 
-    // If the error is a 401 Unauthorized and the request has not been retried yet
-    if (error.response?.status === 401 && config && !config._retry) {
-      config._retry = true;
-      if (refreshToken) {
-        try {
-          const response = await api.post(
-            `${url}/token/refresh/`,
-            {
-              refresh: refreshToken,
-            }
-          );
-      setAuthTokens({ accessToken: response.data.access, refreshToken });
+      // If the error is a 401 Unauthorized and the request has not been retried yet
+      if (error.response?.status === 401 && config && !config._retry) {
+        config._retry = true;
+        if (refreshToken) {
+          try {
+            const response = await instance.post(
+              `/token/refresh/`,
+              {
+                refresh: refreshToken,
+              }
+            );
+            await setAuthTokens({ accessToken: response.data.access, refreshToken });
 
-          config.headers.Authorization = `Bearer ${response.data.access}`;
-          return api(config);
-        } catch (refreshError) {
-          console.error("Refresh token failed:", refreshError);
+            config.headers.Authorization = `Bearer ${response.data.access}`;
+            return instance(config);
+          } catch (refreshError) {
+            console.error("Refresh token failed:", refreshError);
+          }
         }
       }
-    }
 
-    return Promise.reject(error);
-  }
-);
+      return Promise.reject(error);
+    }
+  );
+
+  return instance;
+}
+
 
 export default api;
