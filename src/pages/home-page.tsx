9df68944a7215c7dcb288eeae3630401f3e119dc
Milan Bairagi <nmilan626@gmail.com>
2025-12-22 01:06:21 +0545

    feat: enhance HomePage to fetch and display vault items with error handling
    Fetches password lists from the backend and displays them on the home page.
    
    Introduces state variables to store and manage the fetched password data,
    as well as any error messages that may occur during the fetching process.
    Also, handles potential errors during the API call and displays an error
    message to the user if the password lists cannot be fetched.

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,5 +1,8 @@
+import { useState, useEffect } from "react";
+import { type AxiosResponse } from "axios";
 import { useUserCredential } from "../contexts/useUser";
 import { useAuthCredential } from "../contexts/useAuthCredential";
+import api from "../axios";
 
 
 interface props {
@@ -7,9 +10,20 @@ interface props {
   goToVaultUnlock: () => void;
 };
 
+interface VaultItem {
+  id: number;
+  title: string;
+  username: string;
+  url: string;
+  created_at: string;
+  updated_at: string;
+}
+
 const HomePage: React.FC<props> = ({ goToLogin, goToVaultUnlock }: props) => {
+  const [vaultItems, setVaultItems] = useState<VaultItem[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const { user, isLoading, handleLogout } = useUserCredential() ?? { user: null, isLoading: true, handleLogout: async () => {void 0} };
-  const { vaultUnlockToken } = useAuthCredential();
+  const { accessToken, refreshToken, vaultUnlockToken, setAuthTokens } = useAuthCredential();
 
   if (!vaultUnlockToken) {
     goToVaultUnlock();
@@ -25,16 +39,31 @@ const HomePage: React.FC<props> = ({ goToLogin, goToVaultUnlock }: props) => {
     return null;
   }
 
+  useEffect(() => {
+    fetchPasswordLists();
+  }, []);
+
+  const fetchPasswordLists = async () => {
+    const apiInstance = api(accessToken, refreshToken, vaultUnlockToken, setAuthTokens);
+    
+    try {
+      const res: AxiosResponse<VaultItem[]> = await apiInstance.get("vaults/list-create/");
+      setVaultItems(res.data);
+    } catch (error) {
+      setErrorMessage("Failed to fetch password lists.");
+      console.error("Error fetching password lists:", error);
+    }
+  };
+
   return (
     <div>
       {(user) ? <h3>Welcome! {user.username}</h3> : <h3>Please Login!</h3>}
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
       <div>Password Lists</div>
       <ol>
-        <li>Password 1</li>
-        <li>Password 2</li>
-        <li>Password 3</li>
-        <li>Password 4</li>
-        <li>Password 5</li>
+        {vaultItems.map((item) => (
+          <li key={item.id}>{item.title} - {item.username} - {item.url} - Created at: {item.created_at} - Updated at: {item.updated_at}</li>
+        ))}
       </ol>
 
       <button onClick={handleLogout}>Logout</button>
