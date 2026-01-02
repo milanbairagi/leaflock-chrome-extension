5ade10476c05529e897aa770557e28a7e8d923d0
Milan Bairagi <nmilan626@gmail.com>
2026-01-02 23:58:42 +0545

    feat: Update App layout and add logo to LoginPage
    - Refactored App component to use a styled div instead of fragment and commented out navigation.
    - Added a new SVG logo asset for the Leaflock application.
    - Enhanced LoginPage with a logo and improved styling for input fields and buttons.
    - Updated CSS to include new secondary color variables and adjusted body styling.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,6 +3,7 @@ import type { AxiosResponse } from "axios";
 import api from "../axios";
 import { useAuthCredential, type AuthTokens } from "../contexts/useAuthCredential";
 import { useUserCredential } from "../contexts/useUser";
+import logo from "../assets/images/Logo.svg"
 
 
 interface props {
@@ -45,36 +46,61 @@ const LoginPage: React.FC<props> = ({ goToHome }: props) => {
   };
 
   return (
-    <div>
+    <div className="p-5 rounded-md">
+      <div className="flex justify-center items-center flex-col mb-8">
+        <img src={logo} alt="Leaflock Logo" className="w-40 mx-auto -mb-2" />
+        <p className="text-center">Secure Password Manager</p>
+      </div>
       <form
         onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
           handleLogin(e);
         }}
+        className="flex flex-col gap-4"
       >
-        <h1>Login Page</h1>
         {/* Test */}
-        <p>Access Token: {accessToken}</p>
-        <p>Refresh Token: {refreshToken}</p>
-        <label htmlFor="username">Username: </label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setUsername(e.target.value)
-          }
-        />
-        <br />
-        <label htmlFor="password">Password: </label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setPassword(e.target.value)
-          }
-        />
-        <button type="submit">Login</button>
+        {/* <p>Access Token: {accessToken}</p>
+        <p>Refresh Token: {refreshToken}</p> */}
+
+        <div className="grid gap-1">
+          <label htmlFor="username" className="text-secondary-10">Username: </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setUsername(e.target.value)
+            }
+            placeholder="Username"
+            className="bg-primary-40 text-primary-0 border border-accent-0 rounded-4xl w-full py-2 px-4 focus:outline-none focus:ring-2 focus:ring-accent-40"
+          />
+        </div>
+
+        <div className="grid gap-1">
+          <label htmlFor="password" className="text-secondary-10">Password: </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setPassword(e.target.value)
+            }
+            placeholder="Password"
+            className="bg-primary-40 text-primary-0 border border-accent-0 rounded-4xl w-full py-2 px-4 focus:outline-none focus:ring-2 focus:ring-accent-40"
+            />
+        </div>
+
+        <button
+          type="submit"
+          className="text-center text-white bg-accent-50 rounded-4xl py-2 mt-4 cursor-pointer 
+                      hover:bg-accent-70
+                      disabled:bg-accent-20 disabled:cursor-not-allowed
+                      active:bg-accent-90
+                      transition-colors duration-200 ease-in-out
+          "
+          disabled={!username || !(password.length >= 5)}
+        >
+          Login
+        </button>
       </form>
     </div>
   );
