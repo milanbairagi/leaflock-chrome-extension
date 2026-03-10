ddd6076666e2285aa3b5464d0f43d56f13939210
Milan Bairagi <nmilan626@gmail.com>
2026-03-10 20:15:02 +0545

    feat: enhance Button component with additional props for type and disabled state

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,9 +1,24 @@
-const Button = ({ text, handleClick, styles }: {text: string, handleClick?: () => void, styles?: React.CSSProperties}) => {
+type props = {
+  text: string;
+  handleClick?: () => void;
+  styles?: string;
+  type?: "button" | "submit" | "reset";
+  isDisable?: boolean;
+};
+
+const Button = ({
+  text,
+  handleClick,
+  styles,
+  type,
+  isDisable = false,
+}: props) => {
   return (
     <button
-      type="button"
+      type={type || "button"}
       className={`${styles} text-sm bg-primary-70 text-secondary-10 rounded-md px-2 py-1 hover:bg-primary-50 transition-colors duration-200 ease-in-out`}
       onClick={handleClick}
+      disabled={isDisable}
     >
       {text}
     </button>
