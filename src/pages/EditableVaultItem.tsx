48f9d9f2cf8937489db483638eb00b5bb6dea3c3
Milan Bairagi <nmilan626@gmail.com>
2026-03-10 19:16:07 +0545

    feat: enhance EditableVaultItem layout and add password visibility toggle

--- src/pages/EditableVaultItem.tsx
+++ src/pages/EditableVaultItem.tsx
@@ -4,8 +4,8 @@
  */
 
 import { useState } from "react";
-import Button from "../components/buttons/Button";
 import { type VaultItem } from "../types";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 type props = {
   vaultItem: VaultItem | null;
@@ -48,57 +48,92 @@ const EditableVaultItem = ({
   return (
     <div>
       <form onSubmit={onSubmit}>
-        title:{" "}
-        <input
-          type="text"
-          value={vaultItem?.title}
-          onChange={handleChange}
-          name="title"
-        />
-        <br />
-        username:{" "}
-        <input
-          type="text"
-          value={vaultItem?.username}
-          onChange={handleChange}
-          name="username"
-        />
-        <br />
-        password:{" "}
-        <input
-          type={passwordVisible ? "text" : "password"}
-          value={vaultItem?.password}
-          onChange={handleChange}
-          name="password"
-        />
-        <Button
-          text={passwordVisible ? "Hide" : "Show"}
-          handleClick={togglePasswordVisibility}
-        />
-        <br />
-        url:{" "}
-        <input
-          type="text"
-          value={vaultItem?.url}
-          onChange={handleChange}
-          name="url"
-        />
-        <br />
-        notes:{" "}
-        <textarea
-          value={vaultItem?.notes || ""}
-          onChange={handleChange}
-          name="notes"
-        />
-        <br />
+
+        
+        <div className="bg-primary-40 text-primary-0 w-full py-2 px-4 rounded-md mb-6">
+          <p className="text-primary-0 font-medium text-lg mb-2">Title</p>
+          <input
+            type="text"
+            value={vaultItem?.title}
+            onChange={handleChange}
+            name="title"
+            className="text-xl font-semibold"
+          />
+        </div>
+
+        <p className="text-primary-0 font-medium text-lg mb-2">Login Credential</p>
+        <div className="grid mb-7 bg-primary-40 p-4 rounded-md min-w-0">
+          <div className="flex flex-col border-b-accent-90 border-b pb-1 mb-3 min-w-0">
+            <label htmlFor="username" className="text-md text-secondary-10">Username</label>
+            <input
+              type="text"
+              value={vaultItem?.username}
+              onChange={handleChange}
+              name="username"
+              className="text-lg block w-full min-w-0"
+            />
+          </div>
+          <div className="flex flex-col border-b-accent-90 border-b pb-1 mb-3 min-w-0">
+            <label htmlFor="password" className="text-md text-secondary-10">Password</label>
+            <div className="flex items-center">
+              <input
+                type={passwordVisible ? "text" : "password"}
+                value={vaultItem?.password}
+                onChange={handleChange}
+                name="password"
+                className="text-lg block w-full min-w-0 grow"
+              />
+
+              <div
+                className="flex items-center cursor-pointer pl-2"
+                onClick={togglePasswordVisibility}
+              >
+                {passwordVisible ? (
+                  <FaEye className="h-4 w-4 text-primary-0" />
+                ) : (
+                  <FaEyeSlash className="h-4 w-4 text-primary-0" />
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <p className="text-primary-0 font-medium text-lg mb-2">Autofill Options</p>
+        <div className="bg-primary-40 p-4 rounded-md mb-6">
+          <label htmlFor="url1" className="text-md text-secondary-10 block">Website</label>
+          <input
+            type="text"
+            value={vaultItem?.url}
+            onChange={handleChange}
+            name="url"
+            className="text-md block w-full"
+          />
+        </div>
+
+        <p className="text-primary-0 font-medium text-lg mb-2">Additional Information</p>
+        <div className="bg-primary-40 p-4 rounded-md mb-6">
+          <label htmlFor="notes" className="text-md text-secondary-10 block">Notes</label>
+          <textarea
+            value={vaultItem?.notes || ""}
+            onChange={handleChange}
+            name="notes"
+            className="text-md block w-full"
+          />
+        </div>
+
+        
+
         <button type="submit" disabled={loading}>
           {buttonText}
         </button>
+
+
       </form>
 
       {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
     </div>
   );
 };
+
 
 export default EditableVaultItem;
